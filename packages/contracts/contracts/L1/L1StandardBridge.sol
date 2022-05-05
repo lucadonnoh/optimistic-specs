aa1ceb5348dc35ab484ee6519f23e0ef63a1702f
Kelvin Fichter <kelvin@optimism.io>
2022-05-05 19:31:45 -0400

    wip: again again

--- packages/contracts/contracts/L1/L1StandardBridge.sol
+++ packages/contracts/contracts/L1/L1StandardBridge.sol
@@ -5,8 +5,6 @@ pragma solidity ^0.8.9;
 import { IL2ERC20Bridge } from "@eth-optimism/contracts/L2/messaging/IL2ERC20Bridge.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { L1CrossDomainMessenger } from "./L1CrossDomainMessenger.sol";
-import { IL1StandardBridge } from "../interfaces/IL1StandardBridge.sol";
-import { IL1ERC20Bridge } from "../interfaces/IL1ERC20Bridge.sol";
 
 /* Library Imports */
 import {
@@ -15,6 +13,8 @@ import {
 import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
+import { StandardBridge } from "../universal/StandardBridge.sol";
+
 /**
  * @title L1StandardBridge
  * @dev The L1 ETH and ERC20 Bridge is a contract which stores deposited L1 funds and standard
@@ -22,239 +22,90 @@ import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.s
  * and listening to it for newly finalized withdrawals.
  *
  */
-contract L1StandardBridge is IL1StandardBridge {
-    using SafeERC20 for IERC20;
+contract L1StandardBridge is StandardBridge {
+    /**********
+     * Events *
+     **********/
 
-    /********************************
-     * External Contract References *
-     ********************************/
+    event ETHDepositInitiated(
+        address indexed _from,
+        address indexed _to,
+        uint256 _amount,
+        bytes _data
+    );
 
-    L1CrossDomainMessenger public l1CrossDomainMessenger;
-    address public l2TokenBridge;
+    event ETHWithdrawalFinalized(
+        address indexed _from,
+        address indexed _to,
+        uint256 _amount,
+        bytes _data
+    );
 
-    // Maps L1 token to L2 token to balance of the L1 token deposited
-    mapping(address => mapping(address => uint256)) public deposits;
-
-    /***************
-     * Constructor *
-     ***************/
-
-    // This contract lives behind a proxy, so the constructor parameters will go unused.
-    constructor() {}
-
-    /******************
-     * Initialization *
-     ******************/
-
-    /**
-     * @param _l1CrossDomainMessenger L1CrossDomainMessenger address.
-     * @param _l2TokenBridge L2 standard bridge address.
-     */
-    // slither-disable-next-line external-function
-    function initialize(L1CrossDomainMessenger _l1CrossDomainMessenger, address _l2TokenBridge)
-        public
-    {
-        require(
-            address(_l1CrossDomainMessenger) == address(0),
-            "Contract has already been initialized."
-        );
-        l1CrossDomainMessenger = _l1CrossDomainMessenger;
-        l2TokenBridge = _l2TokenBridge;
-    }
-
-    /**************
-     * Depositing *
-     **************/
-
-    /** @dev Modifier requiring sender to be EOA. This check could be bypassed by a malicious
-     *  contract via initcode, but it takes care of the user error we want to avoid.
-     */
-    modifier onlyEOA() {
-        // Used to stop deposits from contracts (avoid accidentally lost tokens)
-        require(!Address.isContract(msg.sender), "Account not EOA");
-        _;
-    }
-
-    /**
-     * @dev This function can be called with no data
-     * to deposit an amount of ETH to the caller's balance on L2.
-     * Since the receive function doesn't take data, a conservative
-     * default amount is forwarded to L2.
-     */
-    receive() external payable onlyEOA {
-        _initiateETHDeposit(msg.sender, msg.sender, msg.value, 200_000, bytes(""));
-    }
-
-    /**
-     * @inheritdoc IL1StandardBridge
-     */
-    function depositETH(uint32 _l2Gas, bytes calldata _data) external payable onlyEOA {
-        _initiateETHDeposit(msg.sender, msg.sender, msg.value, _l2Gas, _data);
-    }
-
-    /**
-     * @inheritdoc IL1StandardBridge
-     */
-    function depositETHTo(
-        address _to,
-        uint32 _l2Gas,
-        bytes calldata _data
-    ) external payable {
-        _initiateETHDeposit(msg.sender, _to, msg.value, _l2Gas, _data);
-    }
-
-    /**
-     * @dev Performs the logic for deposits by storing the ETH and informing the L2 ETH Gateway of
-     * the deposit.
-     * @param _from Account to pull the deposit from on L1.
-     * @param _to Account to give the deposit to on L2.
-     * @param _l2Gas Gas limit required to complete the deposit on L2.
-     * @param _data Optional data to forward to L2. This data is provided
-     *        solely as a convenience for external contracts. Aside from enforcing a maximum
-     *        length, these contracts provide no guarantees about its content.
-     */
-    function _initiateETHDeposit(
-        address _from,
+    event ERC20DepositInitiated(
+        address indexed _l1Token,
+        address indexed _l2Token,
+        address indexed _from,
         address _to,
         uint256 _amount,
-        uint32 _l2Gas,
-        bytes memory _data
-    ) internal {
-        emit ETHDepositInitiated(_from, _to, _amount, _data);
+        bytes _data
+    );
 
-        // Send calldata into L2
-        l1CrossDomainMessenger.sendMessage{ value: _amount }(
-            Lib_PredeployAddresses.L2_STANDARD_BRIDGE,
-            abi.encodeWithSelector(
-                IL2ERC20Bridge.finalizeDeposit.selector,
-                address(0),
-                Lib_PredeployAddresses.OVM_ETH,
-                _from,
-                _to,
-                _amount,
-                _data
-            ),
-            _l2Gas
-        );
+    event ERC20WithdrawalFinalized(
+        address indexed _l1Token,
+        address indexed _l2Token,
+        address indexed _from,
+        address _to,
+        uint256 _amount,
+        bytes _data
+    );
+
+    /********************
+     * Public Functions *
+     ********************/
+
+    function depositETH(uint32 _minGasLimit, bytes calldata _data) external payable onlyEOA {
+        _initiateETHDeposit(msg.sender, msg.sender, _minGasLimit, _data);
     }
 
-    /**
-     * @inheritdoc IL1ERC20Bridge
-     */
+    function depositETHTo(
+        address _to,
+        uint32 _minGasLimit,
+        bytes calldata _data
+    ) external payable {
+        _initiateETHDeposit(msg.sender, _to, _minGasLimit, _data);
+    }
+
     function depositERC20(
         address _l1Token,
         address _l2Token,
         uint256 _amount,
-        uint32 _l2Gas,
+        uint32 _minGasLimit,
         bytes calldata _data
     ) external virtual onlyEOA {
-        _initiateERC20Deposit(_l1Token, _l2Token, msg.sender, msg.sender, _amount, _l2Gas, _data);
+        _initiateERC20Deposit(_l1Token, _l2Token, msg.sender, msg.sender, _amount, _minGasLimit, _data);
     }
 
-    /**
-     * @inheritdoc IL1ERC20Bridge
-     */
     function depositERC20To(
         address _l1Token,
         address _l2Token,
         address _to,
         uint256 _amount,
-        uint32 _l2Gas,
+        uint32 _minGasLimit,
         bytes calldata _data
     ) external virtual {
-        _initiateERC20Deposit(_l1Token, _l2Token, msg.sender, _to, _amount, _l2Gas, _data);
+        _initiateERC20Deposit(_l1Token, _l2Token, msg.sender, _to, _amount, _minGasLimit, _data);
     }
 
-    /**
-     * @dev Performs the logic for deposits by informing the L2 Deposited Token
-     * contract of the deposit and calling a handler to lock the L1 funds. (e.g. transferFrom)
-     *
-     * @param _l1Token Address of the L1 ERC20 we are depositing
-     * @param _l2Token Address of the L1 respective L2 ERC20
-     * @param _from Account to pull the deposit from on L1
-     * @param _to Account to give the deposit to on L2
-     * @param _amount Amount of the ERC20 to deposit.
-     * @param _l2Gas Gas limit required to complete the deposit on L2.
-     * @param _data Optional data to forward to L2. This data is provided
-     *        solely as a convenience for external contracts. Aside from enforcing a maximum
-     *        length, these contracts provide no guarantees about its content.
-     */
-    function _initiateERC20Deposit(
-        address _l1Token,
-        address _l2Token,
-        address _from,
-        address _to,
-        uint256 _amount,
-        uint32 _l2Gas,
-        bytes calldata _data
-    ) internal {
-        // When a deposit is initiated on L1, the L1 Bridge transfers the funds to itself for future
-        // withdrawals. safeTransferFrom also checks if the contract has code, so this will fail if
-        // from is an EOA or address(0).
-        // slither-disable-next-line reentrancy-events, reentrancy-benign
-        IERC20(_l1Token).safeTransferFrom(_from, address(this), _amount);
-
-        // slither-disable-next-line reentrancy-benign
-        deposits[_l1Token][_l2Token] = deposits[_l1Token][_l2Token] + _amount;
-
-        // slither-disable-next-line reentrancy-events
-        emit ERC20DepositInitiated(_l1Token, _l2Token, _from, _to, _amount, _data);
-
-        // Send calldata into L2
-        l1CrossDomainMessenger.sendMessage(
-            Lib_PredeployAddresses.L2_STANDARD_BRIDGE,
-            abi.encodeWithSelector(
-                IL2ERC20Bridge.finalizeDeposit.selector,
-                _l1Token,
-                _l2Token,
-                _from,
-                _to,
-                _amount,
-                _data
-            ),
-            _l2Gas
-        );
-    }
-
-    /*************************
-     * Cross-chain Functions *
-     *************************/
-
-    /**
-     * @notice Ensures that the caller is the portal, and that it has the l2Sender value
-     * set to the address of the L2 Token Bridge.
-     */
-    modifier onlyL2Bridge() {
-        require(
-            msg.sender == address(l1CrossDomainMessenger),
-            "Messages must be relayed by first calling the Optimism Portal"
-        );
-        require(
-            // TODO(tynes): ensure this is correct
-            l1CrossDomainMessenger.xDomainMessageSender() == l2TokenBridge,
-            "Message must be sent from the L2 Token Bridge"
-        );
-        _;
-    }
-
-    /**
-     * @inheritdoc IL1StandardBridge
-     */
     function finalizeETHWithdrawal(
         address _from,
         address _to,
         uint256 _amount,
         bytes calldata _data
-    ) external payable onlyL2Bridge {
+    ) external payable onlyOtherBridge {
         emit ETHWithdrawalFinalized(_from, _to, _amount, _data);
-
-        (bool success, ) = _to.call{ value: _amount }(new bytes(0));
-        require(success, "TransferHelper::safeTransferETH: ETH transfer failed");
+        finalizeBridgeETH(_from, _to, _amount, _data);
     }
 
-    /**
-     * @inheritdoc IL1ERC20Bridge
-     */
     function finalizeERC20Withdrawal(
         address _l1Token,
         address _l2Token,
@@ -262,25 +113,35 @@ contract L1StandardBridge is IL1StandardBridge {
         address _to,
         uint256 _amount,
         bytes calldata _data
-    ) external onlyL2Bridge {
-        deposits[_l1Token][_l2Token] = deposits[_l1Token][_l2Token] - _amount;
+    ) external onlyOtherBridge {
         emit ERC20WithdrawalFinalized(_l1Token, _l2Token, _from, _to, _amount, _data);
-
-        // When a withdrawal is finalized on L1, the L1 Bridge transfers the funds to the withdrawer
-        IERC20(_l1Token).safeTransfer(_to, _amount);
+        finalizeBridgeERC20(_l1Token, _l2Token, _from, _to, _amount, _data);
     }
 
-    /*****************************
-     * Temporary - Migrating ETH *
-     *****************************/
+    /**********************
+     * Internal Functions *
+     **********************/
 
-    /**
-     * @dev Adds ETH balance to the account. This is meant to allow for ETH
-     * to be migrated from an old gateway to a new gateway.
-     * NOTE: This is left for one upgrade only so we are able to receive the migrated ETH from the
-     * old contract
-     */
-    // Bedrock upgrade note: we may use this if this contract gets merged into the portal,
-    // or even if we just start using a new proxy.
-    function donateETH() external payable {}
+    function _initiateETHDeposit(
+        address _from,
+        address _to,
+        uint32 _minGasLimit,
+        bytes memory _data
+    ) internal {
+        emit ETHDepositInitiated(_from, _to, msg.value, _data);
+        _initiateBridgeETH(_from, _to, msg.value, _minGasLimit, _data);
+    }
+
+    function _initiateERC20Deposit(
+        address _l1Token,
+        address _l2Token,
+        address _from,
+        address _to,
+        uint256 _amount,
+        uint32 _minGasLimit,
+        bytes calldata _data
+    ) internal {
+        emit ERC20DepositInitiated(_l1Token, _l2Token, _from, _to, _amount, _data);
+        _initiateBridgeERC20(_l1Token, _l2Token, _from, _to, _amount, _minGasLimit, _data);
+    }
 }
---
+++ packages/contracts/contracts/universal/StandardBridge.sol
@@ -0,0 +1,251 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+/* Interface Imports */
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/* Library Imports */
+import { ERC165Checker } from "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
+import {
+    Lib_PredeployAddresses
+} from "@eth-optimism/contracts/libraries/constants/Lib_PredeployAddresses.sol";
+import { Address } from "@openzeppelin/contracts/utils/Address.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import { CrossDomainMessenger } from "./CrossDomainMessenger.sol";
+
+abstract contract StandardBridge {
+    using SafeERC20 for IERC20;
+
+    /**********
+     * Events *
+     **********/
+
+    event ETHBridgeInitiated(
+        address indexed _from,
+        address indexed _to,
+        uint256 _amount,
+        bytes _data
+    );
+
+    event ETHBridgeFinalized(
+        address indexed _from,
+        address indexed _to,
+        uint256 _amount,
+        bytes _data
+    );
+
+    event ERC20BridgeInitiated(
+        address indexed _localToken,
+        address indexed _remoteToken,
+        address indexed _from,
+        address _to,
+        uint256 _amount,
+        bytes _data
+    );
+
+    event ERC20BridgeFinalized(
+        address indexed _localToken,
+        address indexed _remoteToken,
+        address indexed _from,
+        address _to,
+        uint256 _amount,
+        bytes _data
+    );
+
+    /*************
+     * Variables *
+     *************/
+
+    CrossDomainMessenger public messenger;
+    StandardBridge public otherBridge;
+
+    mapping(address => mapping(address => uint256)) public deposits;
+
+    /*************
+     * Modifiers *
+     *************/
+
+    modifier onlyEOA() {
+        require(!Address.isContract(msg.sender), "Account not EOA");
+        _;
+    }
+
+    /**
+     * @notice Ensures that the caller is the portal, and that it has the l2Sender value
+     * set to the address of the L2 Token Bridge.
+     */
+    modifier onlyOtherBridge() {
+        require(
+            msg.sender == address(messenger) && messenger.xDomainMessageSender() == address(otherBridge),
+            "Could not authenticate bridge message."
+        );
+        _;
+    }
+
+    /********************
+     * Public Functions *
+     ********************/
+
+    function donateETH() external payable {}
+
+    receive() external payable onlyEOA {
+        _initiateBridgeETH(msg.sender, msg.sender, msg.value, 200_000, bytes(""));
+    }
+
+    function bridgeETH(uint32 _minGasLimit, bytes calldata _data) public payable onlyEOA {
+        _initiateBridgeETH(msg.sender, msg.sender, msg.value, _minGasLimit, _data);
+    }
+
+    function bridgeETHTo(
+        address _to,
+        uint32 _minGasLimit,
+        bytes calldata _data
+    ) public payable {
+        _initiateBridgeETH(msg.sender, _to, msg.value, _minGasLimit, _data);
+    }
+
+    function bridgeERC20(
+        address _localToken,
+        address _remoteToken,
+        uint256 _amount,
+        uint32 _minGasLimit,
+        bytes calldata _data
+    ) public virtual onlyEOA {
+        _initiateBridgeERC20(_localToken, _remoteToken, msg.sender, msg.sender, _amount, _minGasLimit, _data);
+    }
+
+    function bridgeERC20To(
+        address _localToken,
+        address _remoteToken,
+        address _to,
+        uint256 _amount,
+        uint32 _minGasLimit,
+        bytes calldata _data
+    ) public virtual {
+        _initiateBridgeERC20(_localToken, _remoteToken, msg.sender, _to, _amount, _minGasLimit, _data);
+    }
+
+    function finalizeBridgeETH(
+        address _from,
+        address _to,
+        uint256 _amount,
+        bytes calldata _data
+    ) public payable onlyOtherBridge {
+        require(
+            msg.value == _amount,
+            "Amount sent does not match amount required."
+        );
+
+        emit ETHBridgeFinalized(_from, _to, _amount, _data);
+        (bool success, ) = _to.call{ value: _amount }(new bytes(0));
+        require(success, "TransferHelper::safeTransferETH: ETH transfer failed");
+    }
+
+    function finalizeBridgeERC20(
+        address _localToken,
+        address _remoteToken,
+        address _from,
+        address _to,
+        uint256 _amount,
+        bytes calldata _data
+    ) public onlyOtherBridge {
+        if (_isOptimismMintable(_localToken, _remoteToken)) {
+            OptimismMintableERC20(_localToken).mint(_to, _amount);
+        } else {
+            deposits[_localToken][_remoteToken] = deposits[_localToken][_remoteToken] - _amount;
+            IERC20(_localToken).safeTransfer(_to, _amount);
+        }
+
+        emit ERC20BridgeFinalized(_localToken, _remoteToken, _from, _to, _amount, _data);
+    }
+
+    /**********************
+     * Internal Functions *
+     **********************/
+
+    function _initialize(
+        CrossDomainMessenger _messenger,
+        StandardBridge _otherBridge
+    )
+        internal
+    {
+        // TODO: Figure out if we want this behind a proxy with Initializable.
+        require(
+            address(_messenger) == address(0),
+            "Contract has already been initialized."
+        );
+
+        messenger = _messenger;
+        otherBridge = _otherBridge;
+    }
+
+    function _initiateBridgeETH(
+        address _from,
+        address _to,
+        uint256 _amount,
+        uint32 _minGasLimit,
+        bytes memory _data
+    ) internal {
+        emit ETHBridgeInitiated(_from, _to, _amount, _data);
+
+        messenger.sendMessage{ value: _amount }(
+            address(otherBridge),
+            abi.encodeWithSelector(
+                this.finalizeBridgeETH.selector,
+                _from,
+                _to,
+                _amount,
+                _data
+            ),
+            _minGasLimit
+        );
+    }
+
+    function _initiateBridgeERC20(
+        address _localToken,
+        address _remoteToken,
+        address _from,
+        address _to,
+        uint256 _amount,
+        uint32 _minGasLimit,
+        bytes calldata _data
+    ) internal {
+        if (_isOptimismMintable(_localToken, _remoteToken)) {
+            OptimismMintableERC20(_localToken).burn(msg.sender, _amount);
+        } else {
+            IERC20(_localToken).safeTransferFrom(_from, address(this), _amount);
+            deposits[_localToken][_remoteToken] = deposits[_localToken][_remoteToken] + _amount;
+        }
+
+        messenger.sendMessage(
+            address(otherBridge),
+            abi.encodeWithSelector(
+                this.finalizeBridgeERC20.selector,
+                _remoteToken,
+                _localToken,
+                _from,
+                _to,
+                _amount,
+                _data
+            ),
+            _minGasLimit
+        );
+
+        emit ERC20BridgeInitiated(_localToken, _remoteToken, _from, _to, _amount, _data);
+    }
+
+    function _isOptimismMintable(
+        address _localToken,
+        address _remoteToken
+    )
+        internal
+        view
+        returns (bool)
+    {
+        return (
+            (ERC165Checker.supportsInterface(_localToken, 0x1d1d8b63) && _remoteToken == OptimismMintableERC20(_localToken).l1Token()) ||
+            (ERC165Checker.supportsInterface(_localToken, 0xFFFFFFFF) && _remoteToken == OptimismMintableERC20(_localToken).l2Token())
+        );
+    }
+}
